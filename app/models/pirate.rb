01de9eabccfa37e13036f359a3715d7069f99004
newtypeV2 <gianlibunao@gmail.com>
2019-06-03 23:05:29 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+    attr_accessor :name,:weight,:height
+    @@all = []
+
+    def initialize(name,weight,height)
+        @name,@weight,@height = name,weight,height
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+
+    attr_accessor :name,:type,:booty
+    @@all = []
+
+    def initialize(name,type,booty)
+        @name,@type,@booty = name,type,booty
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
